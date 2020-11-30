f563c6f92773d2b0553c426f9789cb4eb121dd61
Waleed Rizwan <w2rizwan@edu.uwaterloo.ca>
2020-11-29 20:58:53 -0500

    works via DOM manipulation

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def index():
     return render_template('index.html')
 
 # gets invoked when user makes HTTP post request to <url>/user/
-@app.route('/user/', methods=['POST'])
+@app.route('/user', methods=['POST', 'OPTIONS', 'GET'])
 def user():
    
     print("User function in main.python has been run ")
@@ -21,17 +21,10 @@ def user():
     resp.headers['Access-Control-Allow-Origin'] = '*'
     # get HTTP request body & use user name for something
     # call your twitter scripts here  
-  
-    print(request.json)
-  
+
+    print(request.args.get('username'))
+    
     def userTweet(arg):
-
-      '''
-      consumes username as a string returns
-      list of last 200 tweets
-
-      '''
-
       # API keys 
       access_key = 'MdLc2PWvaUh4ZCQn0DJESoe4n'
       access_token = 'M7PqFUKvpV2MtnvvLRpo0mvg5ZDQgaa5JlavUxy5YgFlDONEeL'
@@ -46,22 +39,79 @@ def user():
       # user timeline  method consumes a user name and returns the last 200 tweets 
       tweetList = api.user_timeline(screen_name=arg, tweet_mode="extended", count = 200)
 
-      return tweetList
+      actualTextList = []
+
+      for x in tweetList:
+        # remove any instances of @ and RT 
+        text = x.full_text.replace("@", "").replace("RT", "")
+        # removes non alphanumeric characters such as emojis
+        for elm in text:
+            if not(elm.isalnum()) and elm != " ":
+                text = text.replace(elm, "")                 
+        actualTextList.append(text)
+      return actualTextList
+
+    # holds username from site  
+    username = request.args.get('username')
+    recentTweets = userTweet(username)
+       
+    return jsonify(recentTweets)
+
+
+# other routes/API endpoints to add after initial is figured out 
+
+'''
+@app.route('/hashtag', methods=['POST', 'OPTIONS', 'GET'])
+def hashtag():
+
+   print("Hashtag function in main.python has been run ")
+    resp = Response("hashtag pivot")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    # get HTTP request body & use user name for something
+    # call your twitter scripts here  
+
+    print(request.args.get('username'))
+    
+    def userTweet(arg):
+      # API keys 
+      access_key = 'MdLc2PWvaUh4ZCQn0DJESoe4n'
+      access_token = 'M7PqFUKvpV2MtnvvLRpo0mvg5ZDQgaa5JlavUxy5YgFlDONEeL'
+      secret_key = '1309966965663035392-4YY7ERbSPWTFyPoAHwCQr0K7c0FtYC'
+      secret_token = 'RPBhXHjoumebhiURAQ7D5HZtq2EaBFwHsoZKQxn7bBiXo'
+
+      auth = tweepy.OAuthHandler(access_key, access_token)
+      auth.set_access_token(secret_key, secret_token)
+
+      api = tweepy.API(auth)
+
+      # user timeline  method consumes a user name and returns the last 200 tweets 
+      tweetList = api.user_timeline(screen_name=arg, tweet_mode="extended", count = 200)
+
+      actualTextList = []
+
+      for x in tweetList:
+        # remove any instances of @ and RT 
+        text = x.full_text.replace("@", "").replace("RT", "")
+        # removes non alphanumeric characters such as emojis
+        for elm in text:
+            if not(elm.isalnum()) and elm != " ":
+                text = text.replace(elm, "")                 
+        actualTextList.append(text)
+      return actualTextList
 
     # holds username 
-    username = request.json['body']['username']
-
+    username = request.args.get('username')
     recentTweets = userTweet(username)
-
     
-
     # resp is what is returned after the API call 
 
     # todo: return jsonified response
-    return resp
+    #print(recentTweets)
+  
+    return jsonify(recentTweets)
 
-# other routes/API endpoints
-# @app.route('/')
+'''
+
 
 
 app.run(host='0.0.0.0', port=8080)
--- static/index.js
+++ static/index.js
@@ -1,25 +1,60 @@
 console.log('Print test before async func call');
 
-async function getTwitterUser(username) {
+async function getTwitterUser(username, endpoint = 'user') {
   try {
-    var res = await fetch('http://localhost:8080', {
+    var res = await fetch(`http://localhost:8080/${endpoint}?username=${username}`, {
       // this will be your heroku domain
       method: 'POST',
       mode: 'no-cors',
-      body: JSON.stringify({
-        username,
-      }),
-      headers: {
+      // body: JSON.stringify({
+      //   username,
+      // }),
+      cache: "no-cache",
+      headers: new Headers({
         'Content-Type': 'application/json'
-      },
+      }),
       // body: { hello: "world" } 
     })
-    console.log(res);
+
+    // user tweets are stored 
+    var body = await res.json()
+
+    console.log(body);
+    return body
   } catch (err) {
     console.log('Issue with line 20 index.js', err)
   }
 }
 
+// update state t
+/* this.state = {
+  tweets: [];
+}
 
-getTwitterUser('scena360');
-console.log('after all function callls in index.js');
+*/
+
+/*
+const newTweets = await getTweets();
+this.setState({ tweets: newTweets })
+
+render() { return <div>{this.state.tweets.toString()}</div>}
+*/
+
+// async function returns a promise , need to await promise to use response or else tweets will be undefined 
+// get twitter user returns promise, can only be accessed with await
+// how to get text value from input react
+(async function () {
+
+  // must call getTwitterUser on button click from drop down menu  
+  var tweets = await getTwitterUser('scena360', 'user');
+
+  // to access a different function on the API change the end point to the new endpoint with the new function  
+
+  console.log(tweets)
+  var p = document.createElement('p');
+  p.innerText = tweets.toString();
+  var body = document.querySelector('#body')
+  body.appendChild(p)
+})()
+
+console.log('End of index.js');

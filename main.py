8f8c25bdebc4c8e475d64281c35b68c958d083de
Waleed Rizwan <w2rizwan@edu.uwaterloo.ca>
2020-12-05 19:35:01 -0500

    major updates to functionality

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ app = Flask(
 )
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 # gets invoked when user makes HTTP post request to <url>/user/
 @app.route('/user', methods=['POST', 'OPTIONS', 'GET'])
@@ -64,52 +66,15 @@ def user():
 @app.route('/hashtag', methods=['POST', 'OPTIONS', 'GET'])
 def hashtag():
 
-   print("Hashtag function in main.python has been run ")
-    resp = Response("hashtag pivot")
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    # get HTTP request body & use user name for something
-    # call your twitter scripts here  
+  ## gets 200 tweets by hashtag as q parameter 
+   for tweet in tweepy.Cursor(api.search,q="#tesla",tweet_mode="extended", count=1,
+                           lang="en",
+                           since="2017-04-03").items():
+    a = tweet
+    print (tweet.created_at, tweet.full_text)
 
-    print(request.args.get('username'))
-    
-    def userTweet(arg):
-      # API keys 
-      access_key = 'MdLc2PWvaUh4ZCQn0DJESoe4n'
-      access_token = 'M7PqFUKvpV2MtnvvLRpo0mvg5ZDQgaa5JlavUxy5YgFlDONEeL'
-      secret_key = '1309966965663035392-4YY7ERbSPWTFyPoAHwCQr0K7c0FtYC'
-      secret_token = 'RPBhXHjoumebhiURAQ7D5HZtq2EaBFwHsoZKQxn7bBiXo'
-
-      auth = tweepy.OAuthHandler(access_key, access_token)
-      auth.set_access_token(secret_key, secret_token)
-
-      api = tweepy.API(auth)
-
-      # user timeline  method consumes a user name and returns the last 200 tweets 
-      tweetList = api.user_timeline(screen_name=arg, tweet_mode="extended", count = 200)
-
-      actualTextList = []
-
-      for x in tweetList:
-        # remove any instances of @ and RT 
-        text = x.full_text.replace("@", "").replace("RT", "")
-        # removes non alphanumeric characters such as emojis
-        for elm in text:
-            if not(elm.isalnum()) and elm != " ":
-                text = text.replace(elm, "")                 
-        actualTextList.append(text)
-      return actualTextList
-
-    # holds username 
-    username = request.args.get('username')
-    recentTweets = userTweet(username)
-    
-    # resp is what is returned after the API call 
-
-    # todo: return jsonified response
-    #print(recentTweets)
-  
-    return jsonify(recentTweets)
 
 '''
+
 
 app.run(host='0.0.0.0', port=8080)
--- static/index.js
+++ static/index.js
@@ -1,5 +1,7 @@
 console.log('Print test before async func call');
 
+
+
 async function getTwitterUser(username, endpoint = 'user') {
   try {
     var res = await fetch(`http://localhost:8080/${endpoint}?username=${username}`, {
@@ -18,9 +20,9 @@ async function getTwitterUser(username, endpoint = 'user') {
 
     // user tweets are stored 
     var body = await res.json()
+    
+    return body; 
 
-    console.log(body);
-    return body
   } catch (err) {
     console.log('Issue with line 20 index.js', err)
   }
@@ -43,18 +45,63 @@ render() { return <div>{this.state.tweets.toString()}</div>}
 // async function returns a promise , need to await promise to use response or else tweets will be undefined 
 // get twitter user returns promise, can only be accessed with await
 // how to get text value from input react
-(async function () {
 
-  // must call getTwitterUser on button click from drop down menu  
-  var tweets = await getTwitterUser('scena360', 'user');
 
-  // to access a different function on the API change the end point to the new endpoint with the new function  
+// (async function () {
 
-  console.log(tweets)
-  var p = document.createElement('p');
-  p.innerText = tweets.toString();
-  var body = document.querySelector('#body')
-  body.appendChild(p)
-})()
+//   // must call getTwitterUser on button click from drop down menu  
+//   var tweets = await getTwitterUser('scena360', 'user');
+
+//   // to access a different function on the API change the end point to the new endpoint with the new function  
+
+//   console.log(tweets)
+//   var p = document.createElement('p');
+//   p.innerText = tweets.toString();
+//   var body = document.querySelector('#body')
+//   body.appendChild(p)
+// })()
+
+
+async function getInput(){
+
+  var x = document.getElementById("userinput").value;
+  let count = 0
+
+  // holds 200 most recent tweets from user x 
+  tweetArray = await getTwitterUser(x);
+  console.log(tweetArray)
+
+  // Resets table elm, each time load data is pressed
+  var tableElm = document.querySelector('#tablebody');
+  var tableRows = document.querySelectorAll('.tweet');
+  // nukes existing table before creating new from tweetArray
+  tableRows.forEach(row => row.remove());
+
+
+  // revamp return value to return screen name and date and time then tweet as a column of its own 
+
+  // clear all the children.
+  // tableElm.removeChi
+
+  for (i = 0; i < tweetArray.length; i++) {
+
+    console.log("for loop run");
+
+    // if (document.querySelector(`#tweet-${i}`) !== null) {
+    //   continue;
+    // }
+    var temp = document.createElement('tr');
+    temp.className = 'tweet'
+    temp.innerText = "                " + (i + 1) + ":                " + tweetArray[i]
+    temp.id = `tweet-${i}`; // todo replace i with some unique id
+    tableElm.appendChild(temp)    
+
+}
+
+console.log("getInput has run ")
+
+}
+
 
 console.log('End of index.js');
+

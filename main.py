3cd981f25221759897e614fb06f9fbb415402369
Waleed Rizwan <w2rizwan@edu.uwaterloo.ca>
2020-12-12 22:09:40 -0500

    added tweet link feature

--- main.py
+++ main.py
@@ -7,84 +7,66 @@ app = Flask(
   static_folder='static'  # Name of directory for static files)
 )
 
+# hide these on the server bro
+access_key = 'MdLc2PWvaUh4ZCQn0DJESoe4n'
+access_token = 'M7PqFUKvpV2MtnvvLRpo0mvg5ZDQgaa5JlavUxy5YgFlDONEeL'
+secret_key = '1309966965663035392-4YY7ERbSPWTFyPoAHwCQr0K7c0FtYC'
+secret_token = 'RPBhXHjoumebhiURAQ7D5HZtq2EaBFwHsoZKQxn7bBiXo'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 # gets invoked when user makes HTTP post request to <url>/user/
 @app.route('/user', methods=['POST', 'OPTIONS', 'GET'])
 def user():
    
-    print("User function in main.python has been run ")
-  
-    # get HTTP request body & use user name for something
-    # call your twitter scripts here  
 
-    print("username: ", request.args.get('username'))
-    
-    print("selection: ", request.args.get('selection'))
-
-  
     def userTweet(arg):
-
-      # API keys 
-      access_key = 'MdLc2PWvaUh4ZCQn0DJESoe4n'
-      access_token = 'M7PqFUKvpV2MtnvvLRpo0mvg5ZDQgaa5JlavUxy5YgFlDONEeL'
-      secret_key = '1309966965663035392-4YY7ERbSPWTFyPoAHwCQr0K7c0FtYC'
-      secret_token = 'RPBhXHjoumebhiURAQ7D5HZtq2EaBFwHsoZKQxn7bBiXo'
-
+      print("User tweet func run")
+      # requires multiple authorization methods
       auth = tweepy.OAuthHandler(access_key, access_token)
       auth.set_access_token(secret_key, secret_token)
-
+      # Once authorized, API  object can utilize all Tweepy methods
       api = tweepy.API(auth)
 
       # user timeline  method consumes a user name and returns the last 200 status objects  
-      tweetList = api.user_timeline(screen_name=arg , count = 200 ,tweet_mode="extended")
-      actualTextList = []
+      userTweetList = api.user_timeline(screen_name=arg , count = 200 ,tweet_mode="extended")
+          
+      # Holds list of TweetDict
+      tweetList = []
 
-      for x in tweetList:
-
-        tempDict = {}
-
-       
-        text = x.full_text
+      for tweet in userTweetList:
         
-        date = x.created_at
-        username = x.author.screen_name
-        favorites = x.favorite_count
-        retweets = x.retweet_count
-    
+        # For each Tweet, holds date, text, username, favorite count, retweet count
+        tweetDict = {}
 
-        tempDict['tweet'] = text
+        # Tweepy Status Object Attributes
+        text = tweet.full_text # full Tweet Text
+        date = tweet.created_at # Date Tweeted
+        username = tweet.author.screen_name # Twitter Handle of author
+        favorites = tweet.favorite_count # number of favorites 
+        retweets = tweet.retweet_count
+        tweetID = tweet.id_str # a tweet id is used to generate the URL hyperlink
+        
+        tweetDict['tweet'] = text
         
         # Retweets Require the usage of retweeded_status to retreive the full text
         if(text[0:2]) == "RT":
-          tempDict['tweet'] = x.retweeted_status.full_text
+          tweetDict['tweet'] = tweet.retweeted_status.full_text
 
-
-        tempDict['date'] = date
-        tempDict['username'] = username
-        tempDict['favorites'] = favorites
-        tempDict['retweets'] = retweets
+        tweetDict['date'] = date
+        tweetDict['username'] = username
+        tweetDict['favorites'] = favorites
+        tweetDict['retweets'] = retweets
+        tweetDict['id'] = tweetID
+        tweetList.append(tweetDict)
       
-
-        actualTextList.append(tempDict)
-
-      # return list of dict 
-      
-      return actualTextList
+      print("end of user tweet function")
+      return tweetList
       
     def byHashTag(hashtag):
-
-
-      # API keys 
-      access_key = 'MdLc2PWvaUh4ZCQn0DJESoe4n'
-      access_token = 'M7PqFUKvpV2MtnvvLRpo0mvg5ZDQgaa5JlavUxy5YgFlDONEeL'
-      secret_key = '1309966965663035392-4YY7ERbSPWTFyPoAHwCQr0K7c0FtYC'
-      secret_token = 'RPBhXHjoumebhiURAQ7D5HZtq2EaBFwHsoZKQxn7bBiXo'
 
       auth = tweepy.OAuthHandler(access_key, access_token)
       auth.set_access_token(secret_key, secret_token)
@@ -93,37 +75,27 @@ def user():
       
       tempParam = "#" + hashtag
 
-      actualTextList = []
+      tweetList = []
 
       for tweet in tweepy.Cursor(api.search,q=tempParam, count=200, tweet_mode="extended").items():
           
-          tempDict = {}       
-          tempDict['tweet'] = tweet.full_text
+          tweetDict = {}       
+          tweetDict['tweet'] = tweet.full_text
 
-          if(tempDict['tweet']) == "RT":   
-            tempDict['tweet'] = tweet.retweeted_status.full_text
-
-
-          tempDict['date'] = tweet.created_at
-          tempDict['username'] = tweet.author.screen_name
-          tempDict['favorites'] = tweet.favorite_count
-          tempDict['retweets'] = tweet.retweet_count
+          if(tweetDict['tweet']) == "RT":   
+            tweetDict['tweet'] = tweet.retweeted_status.full_text
 
 
-          actualTextList.append(tempDict)
+          tweetDict['date'] = tweet.created_at
+          tweetDict['username'] = tweet.author.screen_name
+          tweetDict['favorites'] = tweet.favorite_count
+          tweetDict['retweets'] = tweet.retweet_count
+          tweetDict['id'] = tweet.id_str
+          tweetList.append(tweetDict)
           
-      return actualTextList
-
-
+      return tweetList
 
     def byLikedTweets(username):
-
-
-      # API keys 
-      access_key = 'MdLc2PWvaUh4ZCQn0DJESoe4n'
-      access_token = 'M7PqFUKvpV2MtnvvLRpo0mvg5ZDQgaa5JlavUxy5YgFlDONEeL'
-      secret_key = '1309966965663035392-4YY7ERbSPWTFyPoAHwCQr0K7c0FtYC'
-      secret_token = 'RPBhXHjoumebhiURAQ7D5HZtq2EaBFwHsoZKQxn7bBiXo'
 
       auth = tweepy.OAuthHandler(access_key, access_token)
       auth.set_access_token(secret_key, secret_token)
@@ -132,45 +104,47 @@ def user():
       
       tempParam = "#" + username
 
-      actualTextList = []
+      tweetList = []
 
       favorites = api.favorites(screen_name='elonmusk', tweet_mode="extended", count = 200) 
 
       for tweet in favorites:
 
-          tempDict = {}       
-          tempDict['tweet'] = tweet.full_text
+          tweetDict = {}       
+          tweetDict['tweet'] = tweet.full_text
 
-          if(tempDict['tweet']) == "RT":   
-            tempDict['tweet'] = tweet.retweeted_status.full_text
+          if(tweetDict['tweet']) == "RT":   
+            tweetDict['tweet'] = tweet.retweeted_status.full_text
             
-          tempDict['date'] = tweet.created_at
-          tempDict['username'] = tweet.author.screen_name
-          tempDict['favorites'] = tweet.favorite_count
-          tempDict['retweets'] = tweet.retweet_count
+          tweetDict['date'] = tweet.created_at
+          tweetDict['username'] = tweet.author.screen_name
+          tweetDict['favorites'] = tweet.favorite_count
+          tweetDict['retweets'] = tweet.retweet_count
+          tweetDict['id'] = tweet.id_str
+          tweetList.append(tweetDict)
           
-          actualTextList.append(tempDict)
-          
-      return actualTextList
+      return tweetList
 
     username = request.args.get('username')
     selection = request.args.get('selection')
    
+    # Conditional Flow
+    
+    # if user selects tweets by username, call userTweet Function
     if selection == "username":
       recentTweets = userTweet(username)
-      print(recentTweets)
+   
+    # if user selects tweets by hashtag, call byHashTag Function  
     elif selection == "hashtag":
       recentTweets = byHashTag(username)
-      print(recentTweets)
+
+    # if user selects liked tweets by username call byLikedTweets function   
     elif selection == "Liked Tweets By Username":
-
       recentTweets = byLikedTweets(username)
-      print(recentTweets)
-
-
+      
+    # Returns jsonified array of dictionaries, containing all Tweet data
     return jsonify(recentTweets)
 
 
-
-
+# hosted on local server
 app.run(host='0.0.0.0', port=8080)
--- static/index.js
+++ static/index.js
@@ -21,26 +21,26 @@ async function getTwitterUser(username, selection, endpoint = 'user') {
 }
 
 
+// formats any large number with commas and appropriate spacing 
 function formatNumber(num) {
 
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 
-
 }
+
 
 async function getInput(e){
 
-  // holds selection of how to retrieve the Tweets
+  // Returns drop down menu selection of how to retreive Tweets
   var dataChoice = document.getElementById("selectTag").value;
 
 
-  // if user selects by username retrieve last 200 tweets of the given username or none if not found
+  // if user selects filter by username retrieve last 200 tweets of the given username or none if not found
   if (dataChoice == "Username") {
-    //  block of code to be executed if the condition is true
-
+    
     var x = document.getElementById("userinput").value;
   
-    // holds 200 most recent tweets from user x 
+    // holds 200 most recent tweets from user 'x' 
     tweetArray = await getTwitterUser(x, "username");
     console.log(tweetArray)
   
@@ -51,45 +51,67 @@ async function getInput(e){
     // nukes existing table rows before creating new from tweetArray
     tableRows.forEach(row => row.remove());
   
-  
-
+    // iterates over array containing all Tweets
     for (i = 0; i < tweetArray.length; i++) {
-  
-      console.log("for loop run");
-  
-      var temp1 = document.createElement('tr');
-      temp1.className = 'tweet'
-      var temp2 = document.createElement('td');
-      temp2.className = 'tweet'
-      temp2.innerText = tweetArray[i]["date"]
-      var temp3 = document.createElement('td');
-      temp3.className = 'tweet'
-      temp3.innerText = tweetArray[i]["username"]
-      var temp4 = document.createElement('td');
-      temp4.className = 'tweet'
-      temp4.innerText = tweetArray[i]["tweet"]
-
-      var temp5 = document.createElement('td');
-      temp5.className = 'tweet'
-      temp5.innerText = formatNumber(tweetArray[i]["favorites"])
-
-      var temp6 = document.createElement('td');
-      temp6.className = 'tweet'
-      temp6.innerText = formatNumber(tweetArray[i]["retweets"])
-
-
-      temp1.appendChild(temp2)
-      temp1.appendChild(temp3)
-      temp1.appendChild(temp4)
-      temp1.appendChild(temp5)   
-      temp1.appendChild(temp6)
-      tableElm.appendChild(temp1)
       
+      // href link so user can click row and redirect to actual Tweet itself 
+      var tweetLink = document.createElement('a');
+      tweetLink.className = 'tweet'
+      // the link is created using this url and adding the Tweet's ID to the end 
+      tweetLink.href = 'https://twitter.com/twitter/statuses/' + tweetArray[i]["id"]
+      // setting the target to blank so that it opens in a new window 
+      tweetLink.target = '_blank'
+    
+        
+      // Creates new row tag to hold Tweet Data
+      var newRow = document.createElement('tr');
+      newRow.className = 'tweet'
+      
+      // creates column to hold Date 
+      var date = document.createElement('td');
+      date.className = 'tweet'    
+      date.innerText = tweetArray[i]["date"]
+     
+      // creates column to hold the username
+      var handle = document.createElement('td');
+      handle.className = 'tweet'
+      handle.innerText = tweetArray[i]["username"]
+      
+      // creates column to hold the text of the Tweet
+      var tweetText = document.createElement('td');
+      tweetText.className = 'tweet'
+      tweetText.innerText = tweetArray[i]["tweet"]
+
+      // creates column to hold favorite count   
+      var favoriteCount = document.createElement('td');
+      favoriteCount.className = 'tweet'
+      favoriteCount.innerText = formatNumber(tweetArray[i]["favorites"])
+
+      // creates column to hold Retweet count
+      var retweetCount = document.createElement('td');
+      retweetCount.className = 'tweet'
+      retweetCount.innerText = formatNumber(tweetArray[i]["retweets"])
+
+
+      // adds columns to new row
+      newRow.appendChild(date)
+      newRow.appendChild(handle)
+      
+      // wraps text column in a tag for href, then add the <a> to the row itself
+      tweetLink.appendChild(tweetText)
+      newRow.appendChild(tweetLink)
+      
+      newRow.appendChild(favoriteCount)   
+      newRow.appendChild(retweetCount)
+      
+      // adds new row to existing table element 
+      tableElm.appendChild(newRow)
+
   
   }
     
-
-
+      
+// if user selects Twwets by Hashtag, retrieve last 200 tweets containing the hashtag or none if not found 
   } else if (dataChoice == "Hashtag") {
     
     var x = document.getElementById("userinput").value;
@@ -104,44 +126,66 @@ async function getInput(e){
     // nukes existing table rows before creating new from tweetArray
     tableRows.forEach(row => row.remove());
   
+    // iterates over array containing all relevant Tweet information
     for (i = 0; i < tweetArray.length; i++) {
   
-      console.log("for loop run");
   
-      var temp1 = document.createElement('tr');
-      temp1.className = 'tweet'
-      var temp2 = document.createElement('td');
-      temp2.className = 'tweet'
-      temp2.innerText = tweetArray[i]["date"]
-      var temp3 = document.createElement('td');
-      temp3.className = 'tweet'
-      temp3.innerText = tweetArray[i]["username"]
-      var temp4 = document.createElement('td');
-      temp4.className = 'tweet'
-      temp4.innerText = tweetArray[i]["tweet"]
-  
+      // href link so user can click row and redirect to actual Tweet itself 
+      var tweetLink = document.createElement('a');
+      tweetLink.className = 'tweet'
+      // the link is creating using this url and adding the Tweets ID to the end 
+      tweetLink.href = 'https://twitter.com/twitter/statuses/' + tweetArray[i]["id"]
+      // setting the target to blank so that it opens in a new window 
+      tweetLink.target = '_blank'
+    
+        
+      // Creates new row tag to hold Tweet Data
+      var newRow = document.createElement('tr');
+      newRow.className = 'tweet'
+      
+      // creates column to hold Date 
+      var date = document.createElement('td');
+      date.className = 'tweet'    
+      date.innerText = tweetArray[i]["date"]
+     
+      // creates column to hold the username
+      var handle = document.createElement('td');
+      handle.className = 'tweet'
+      handle.innerText = tweetArray[i]["username"]
+      
+      // creates column to hold the text of the Tweet
+      var tweetText = document.createElement('td');
+      tweetText.className = 'tweet'
+      tweetText.innerText = tweetArray[i]["tweet"]
 
-      var temp5 = document.createElement('td');
-      temp5.className = 'tweet'
-      temp5.innerText = formatNumber(tweetArray[i]["favorites"])
+      // creates column to hold favorite count   
+      var favoriteCount = document.createElement('td');
+      favoriteCount.className = 'tweet'
+      favoriteCount.innerText = formatNumber(tweetArray[i]["favorites"])
 
-      var temp6 = document.createElement('td');
-      temp6.className = 'tweet'
-      temp6.innerText = formatNumber(tweetArray[i]["retweets"])
+      // creates column to hold Retweet count
+      var retweetCount = document.createElement('td');
+      retweetCount.className = 'tweet'
+      retweetCount.innerText = formatNumber(tweetArray[i]["retweets"])
 
 
-      temp1.appendChild(temp2)
-      temp1.appendChild(temp3)
-      temp1.appendChild(temp4)
-      temp1.appendChild(temp5)   
-      temp1.appendChild(temp6)
-      tableElm.appendChild(temp1)
-  
+      // adds columns to new row
+      newRow.appendChild(date)
+      newRow.appendChild(handle)
+      
+      // wraps text column in a tag for href, then add the <a> to the row itself
+      tweetLink.appendChild(tweetText)
+      newRow.appendChild(tweetLink)
+      
+      newRow.appendChild(favoriteCount)   
+      newRow.appendChild(retweetCount)
+      
+      // adds new row to existing table element 
+      tableElm.appendChild(newRow)
   }
     
 
-
-
+// if user selects Liked Tweets By username, retrieve last 200 liked Tweets of the user or 0 if none exist or if the user does not exist
   }else if (dataChoice == "Liked Tweets By Username") {
     
     var x = document.getElementById("userinput").value;
@@ -157,48 +201,65 @@ async function getInput(e){
     // nukes existing table rows before creating new from tweetArray
     tableRows.forEach(row => row.remove());
   
+    // iterates over array containing all relevant Tweet information  
     for (i = 0; i < tweetArray.length; i++) {
   
-      console.log("for loop run");
-  
-      var temp1 = document.createElement('tr');
-      temp1.className = 'tweet'
-      var temp2 = document.createElement('td');
-      temp2.className = 'tweet'
-      temp2.innerText = tweetArray[i]["date"]
-      var temp3 = document.createElement('td');
-      temp3.className = 'tweet'
-      temp3.innerText = tweetArray[i]["username"]
-      var temp4 = document.createElement('td');
-      temp4.className = 'tweet'
-      temp4.innerText = tweetArray[i]["tweet"]
-  
-      var temp5 = document.createElement('td');
-      temp5.className = 'tweet'
-      temp5.innerText = formatNumber(tweetArray[i]["favorites"])
+        // href link so user can click row and redirect to actual Tweet itself 
+        var tweetLink = document.createElement('a');
+        tweetLink.className = 'tweet'
+        // the link is creating using this url and adding the Tweets ID to the end 
+        tweetLink.href = 'https://twitter.com/twitter/statuses/' + tweetArray[i]["id"]
+        // setting the target to blank so that it opens in a new window 
+        tweetLink.target = '_blank'
+      
+          
+        // Creates new row tag to hold Tweet Data
+        var newRow = document.createElement('tr');
+        newRow.className = 'tweet'
+        
+        // creates column to hold Date 
+        var date = document.createElement('td');
+        date.className = 'tweet'    
+        date.innerText = tweetArray[i]["date"]
+        
+        // creates column to hold the username
+        var handle = document.createElement('td');
+        handle.className = 'tweet'
+        handle.innerText = tweetArray[i]["username"]
+        
+        // creates column to hold the text of the Tweet
+        var tweetText = document.createElement('td');
+        tweetText.className = 'tweet'
+        tweetText.innerText = tweetArray[i]["tweet"]
 
-      var temp6 = document.createElement('td');
-      temp6.className = 'tweet'
-      temp6.innerText = formatNumber(tweetArray[i]["retweets"])
+        // creates column to hold favorite count   
+        var favoriteCount = document.createElement('td');
+        favoriteCount.className = 'tweet'
+        favoriteCount.innerText = formatNumber(tweetArray[i]["favorites"])
+
+        // creates column to hold Retweet count
+        var retweetCount = document.createElement('td');
+        retweetCount.className = 'tweet'
+        retweetCount.innerText = formatNumber(tweetArray[i]["retweets"])
 
 
-      temp1.appendChild(temp2)
-      temp1.appendChild(temp3)
-      temp1.appendChild(temp4)
-      temp1.appendChild(temp5)   
-      temp1.appendChild(temp6)
-      tableElm.appendChild(temp1)
-  }
+        // adds columns to new row
+        newRow.appendChild(date)
+        newRow.appendChild(handle)
+        
+        // wraps text column in a tag for href, then add the <a> to the row itself
+        tweetLink.appendChild(tweetText)
+        newRow.appendChild(tweetLink)
+        
+        newRow.appendChild(favoriteCount)   
+        newRow.appendChild(retweetCount)
+        
+        // adds new row to existing table element 
+        tableElm.appendChild(newRow)
 
-
-  }
-
-
- 
-
-
-
-}
+         } 
+   }
+} // Ends Conditional Block
 
 console.log(document.all)
 

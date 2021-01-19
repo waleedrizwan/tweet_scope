7cd355027650f1edcaa8d5d66b4a549a727b5a8c
Waleed Rizwan <w2rizwan@edu.uwaterloo.ca>
2021-01-19 18:07:54 -0500

    testing google cloud NLP API

---
+++ test.py
@@ -0,0 +1,16 @@
+# Imports the Google Cloud client library
+from google.cloud import language_v1
+
+
+# Instantiates a client
+client = language_v1.LanguageServiceClient()
+
+# The text to analyze
+text = u"Hello, world!"
+document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+# Detects the sentiment of the text
+sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+
+print("Text: {}".format(text))
+print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
